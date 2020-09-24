00bc921f3bbac9ffec565dcfd891b7d58a09f99e
Quinten36 <codestuff4all@gmail.com>
2020-09-24 20:33:33 +0200

    begin afl 7

--- app.js
+++ app.js
@@ -133,6 +133,9 @@ Bullet.update = function() {
 	return pack;
 }
 
+//ste to false on live server
+var DEBUG = true;
+
 var io = require('socket.io') (serv, {});
 io.sockets.on('connection', function(socket) {
 	socket.id = Math.random();
@@ -144,25 +147,34 @@ io.sockets.on('connection', function(socket) {
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);
 	});
-	
-    console.log('socket connection');
 
-    socket.on('happy', function(data){
-        console.log(data.reason);
-    })
+	socket.on('sendMsgToServer',function(data){
+		var playerName = (""+socket.id).slice(2,7);
+		for(var i in SOCKET_LIST) {
+			SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
+		}
+  });
+  
+  socket.on('evalServer',function(data){
+    if (!DEBUG)
+      return;
+    var res = eval(data);
+    socket.emit('evalAnswer', res);
+	});
+	
+  console.log('socket connection');
+
+  socket.on('happy', function(data){
+      console.log(data.reason);
+  })
 });
 
 setInterval(function(){
-<<<<<<< HEAD
-	var pack = Player.update();
-
-=======
   var pack = {
     player: Player.update(),
     bullet: Bullet.update()
   }
 	
->>>>>>> f6e6c6444d4ef0b6f4a8f0d58953433d125cd1b0
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
 		socket.emit('newPositions',pack);
